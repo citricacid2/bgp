7fefa7b35f6828641644d61ee2fac65fa20efe61
Ayush <paulayush@gmail.com>
2022-01-04 19:07:46 +0000

    added stuff

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,36 @@
 import Head from "next/head";
 
 export default function Home() {
+  let questions = [
+    {
+      question: "what is the solution to the french revolution",
+      answer: "lorem ipsum",
+    },
 
-  let questions = {}
+  ];
 
   return (
-    <div>
-      <div className="container mx-auto grid place-content-center h-screen bg-gray-200 grid-cols-3">
-        <div className="relative col-start-2">
-          <div className="rounded-2xl shadow-2xl p-5 bg-white absolute top-0 left-0 rotate-2">
-            <h2 className="font-bold text-xl">Question #1</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Perspiciatis accusamus iure similique ipsa ipsam officia in labore
-              a. Quo tempore ipsa blanditiis nulla inventore ratione repellendus
-              voluptas optio iure praesentium?
-            </p>
+    <div className="bg-gray-200">
+      <div className="container mx-auto grid place-content-center h-screen grid-cols-3">
+        <div className="relative col-start-2 h-52">
+          {questions.map((q) => (
+            <div>
+              <div className="rounded-2xl shadow-2xl p-5 bg-white h-52">
+                <h2 className="font-bold text-xl">Question #1</h2>
+                <p>{q.question}</p>
+                <div className="flex flex-row">
+                  <input
+                    type="text"
+                    placeholder="Your answer..."
+                    className="my-2 p-2 border-2 rounded-xl flex-grow"
+                  />
+                  <button className="color px-4 m-2 py-2 text-white rounded-xl bg-blue-400">
+                    Submit
+                  </button>
+                </div>
+              </div>
           </div>
-          <div className="rounded-2xl shadow-2xl p-5 bg-white absolute top-0 left-0 rotate-3">
-            <h2 className="font-bold text-xl">Question #1</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Perspiciatis accusamus iure similique ipsa ipsam officia in labore
-              a. Quo tempore ipsa blanditiis nulla inventore ratione repellendus
-              voluptas optio iure praesentium?
-            </p>
-          </div>
-          <div className="rounded-2xl shadow-2xl p-5 bg-white absolute top-0 left-0">
-            <h2 className="font-bold text-xl">Question #1</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Perspiciatis accusamus iure similique ipsa ipsam officia in labore
-              a. Quo tempore ipsa blanditiis nulla inventore ratione repellendus
-              voluptas optio iure praesentium?
-            </p>
-            <div className="flex flex-row">
-              <input
-                type="text"
-                placeholder="Your answer..."
-                className="my-2 p-2 border-2 rounded-xl flex-grow"
-              />
-              <button className="color px-4 m-2 py-2 text-white rounded-xl bg-blue-400">
-                Submit
-              </button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
